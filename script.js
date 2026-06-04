2e71c4ecc8087482fb93c3e02dbaab747e49267b
gatha <gatha.prasitha@gmail.com>
2026-06-04 12:41:48 +0530

    feat: update

--- script.js
+++ script.js
@@ -14,8 +14,15 @@ function deleteLast() {
 
 function calculate() {
     try {
-        display.value = eval(display.value);
+        let expression = display.value;
+        const openCount = (expression.match(/\(/g) || []).length;
+        const closeCount = (expression.match(/\)/g) || []).length;
+        if (openCount > closeCount) {
+            expression += ")".repeat(openCount - closeCount);
+        }
+        expression = expression.replace(/√\(/g, 'Math.sqrt(');
+        display.value = eval(expression);
     } catch {
         display.value = "Error";
-    }
+    }
 }
